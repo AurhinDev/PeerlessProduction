a76c4bd09ff96aa96cbd51220086ad169981dc7a
AntonErikssonCode <antonerikssonoboy@gmail.com>
2022-09-04 22:57:57 +0200

    Fetch Peer Data Works

--- src/App.js
+++ src/App.js
@@ -17,41 +17,65 @@ function App() {
   const peer_abi = peerABI;
   const token_abi = tokenABI;
 
-  const connect = async () => {
+/*   const connect = async () => { */
+  async function connectToMetamask(){
     const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
     await provider.send("eth_requestAccounts", []);
-    const signer = provider.getSigner();
-    
+    const signer =  provider.getSigner();
+  
     // Denna koden crashar också men används inte till något ännu
     // const myConnectedAddress = provider.selectedAddress ? provider.selectedAddress : provider?.accounts[0];
-  
-    const token = new ethers.Contract("0x592A6983Ff361f5C74e1B63Bd66059cbc3c05358", token_abi, signer)
-    const factory = new ethers.Contract("0xdAC17F958D2ee523a2206206994597C13D831ec7", factory_abi, signer)
-    const peer = new ethers.Contract("VI HAR INGEN ADDRESS ÄNNU", peer_abi, signer)
-    
+    const address = await signer.getAddress();
+    const token = new ethers.Contract("0x7b12Aa06509141AADeabB6CA200ce01a0F0b2B3E", token_abi, signer)
+    const factory = new ethers.Contract("0x592A6983Ff361f5C74e1B63Bd66059cbc3c05358", factory_abi, signer)
+    const peer1 = await factory.peerById(0) 
+    const peerAdress = peer1[1];
+    const peer = new ethers.Contract(peerAdress, peer_abi, signer)
+   console.dir(await peer.GetCostWithFee(1000))
     // Denna koden crashar
-    // const peer1 = await factory.peerById(0) 
-    // console.dir(peer1)
     
     
-    const address = await signer.getAddress()
-    console.dir(await provider.getCode(address))
-    //https://ethereum.stackexchange.com/question s/120817/how-to-call-a-contract-function-method-using-ethersjs
-};
- 
+   /*  console.dir(signer) */
+   /*  console.dir(signer._address) */
+/*      const address = await signer.getAddress()
+ */        /* console.log(address) */
+/*    console.dir(await provider.getCode(address))
+ */
+/* console.dir(await provider.getCode(address))
+ *//* console.dir(signer)
+   console.log("Account:", await signer.getAddress()); */
+     //https://ethereum.stackexchange.com/question s/120817/how-to-call-a-contract-function-method-using-ethersjs
 
-connect()
+    /*  struct PeerStruct {
+      address owner;
+      address peerAdr;
+      address tokenAdr;
+      uint peerID;
+      uint dateCreated;
+      string name;
+      bool frozen;
+  } */
+    };
 
 
 
- 
+/*  async function connectToMetamask(){
+  const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+  // Prompt user for account connections
+  await provider.send("eth_requestAccounts", []);
+  const signer = provider.getSigner();
+  console.log("Account:", await signer.getAddress());
+}
+connectToMetamask()  */
+
+connectToMetamask()
   return (
     <NetworkContext.Provider value={{ network, setNetwork }}>
       <CurrencyContext.Provider value={{ currency, setCurrency }}>
       
       <div className="App">
-   {/*    <button onClick={connect()}> Click </button>  */}
-        <Layout />
+{/*       <button onClick={connect} className="button-23 connect2"> click  </button>  
+ */}        <Layout />
       </div>
 
       </CurrencyContext.Provider >
